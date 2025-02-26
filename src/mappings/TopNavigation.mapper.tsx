c7fea7225c69c3b5b4e01f15de3714df6a64ba02
joshthomas <jthoms1@gmail.com>
2025-02-26 11:21:15 -0600

    top nav

--- src/mappings/TopNavigation.mapper.tsx
+++ src/mappings/TopNavigation.mapper.tsx
@@ -1,61 +1,79 @@
 import { figmaMapping } from "@builder.io/dev-tools/figma";
-import { TopNavigation } from "@/builder-components";
+import {
+  TopNavigation,
+  Input,
+  type TopNavigationProps,
+} from "@cloudscape-design/components";
 
 figmaMapping({
   componentKey: "56a35954cce8f53e9b417c0ef7d35fc9899363bc",
   mapper(figma) {
+    let utilities: TopNavigationProps.Utility[] = [];
+    if (figma.$findOneByName("External link")) {
+      utilities.push({
+        type: "button",
+        text: "Link",
+        href: "https://example.com/",
+        external: true,
+        externalIconAriaLabel: " (opens in a new tab)",
+      });
+    }
+    if (figma.Notifications) {
+      utilities.push({
+        type: "button",
+        iconName: "notification",
+        ariaLabel: "Notifications",
+        badge: true,
+        disableUtilityCollapse: true,
+      });
+    }
+    if (figma["Utility control"]) {
+      utilities.push({
+        type: "button",
+        iconName: "settings",
+        title: "Settings",
+        ariaLabel: "Settings",
+      });
+    }
+    if (figma.$findOneByName("Customer profile")) {
+      utilities.push({
+        type: "menu-dropdown",
+        text: "Customer name",
+        description: "customer@example.com",
+        iconName: "user-profile",
+        items: [],
+      });
+    }
+
+    const identityTitle = figma.$findOneByName("Service name")?.$textContent;
+    const logoImage = figma.$findOneByName("Logo");
+    const logoText = figma.$findOneByName("Logo")?.$textContent;
+
     return (
       <TopNavigation
         identity={{
-          title:
-            figma.$findOneByName("Service name")?.$textContent || "Service",
           href: "#",
+          title: identityTitle || undefined,
+          logo: !logoImage
+            ? undefined
+            : {
+                src: logoImage.$imageUrl,
+                alt: logoText,
+              },
         }}
-        utilities={[
-          {
-            type: "button",
-            text: "Link",
-            href: "https://example.com/",
-            external: true,
-            externalIconAriaLabel: " (opens in a new tab)",
-          },
-          {
-            type: "button",
-            iconName: "notification",
-            title: "Notifications",
-            ariaLabel: "Notifications (unread)",
-            badge: true,
-            disableUtilityCollapse: false,
-          },
-          {
-            type: "menu-dropdown",
-            iconName: "settings",
-            ariaLabel: "Settings",
-            title: "Settings",
-            items: [
-              {
-                id: "settings-org",
-                text: "Organizational settings",
-              },
-              {
-                id: "settings-project",
-                text: "Project settings",
-              },
-            ],
-          },
-          {
-            type: "menu-dropdown",
-            text: "Customer Name",
-            description: "email@example.com",
-            iconName: "user-profile",
-            items: [
-              { id: "profile", text: "Profile" },
-              { id: "preferences", text: "Preferences" },
-              { id: "security", text: "Security" },
-              { id: "signout", text: "Sign out" },
-            ],
-          },
-        ]}
+        search={
+          figma.Search &&
+          figma.$findOneByName("Search") && (
+            <Input
+              ariaLabel="Input field"
+              clearAriaLabel="Clear"
+              value=""
+              type="search"
+              placeholder="Search"
+            />
+          )
+        }
+        utilities={utilities}
       />
     );
   },
